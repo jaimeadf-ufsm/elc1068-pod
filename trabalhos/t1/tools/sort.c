2ab184d7109dd8e3220c0f36f5a08608ee5a5f84
Luis Gustavo Werle Tozevich <luisgustavowt@gmail.com>
2024-05-30 23:30:15 -0300

    refactor(trabalhos/t1): optimize sort function

--- trabalhos/t1/tools/sort.c
+++ trabalhos/t1/tools/sort.c
@@ -6,8 +6,8 @@
 
 int compare(const void *a, const void *b)
 {
-    int *int_a = (int*)a;
-    int *int_b = (int*)b;
+    int *int_a = (int *)a;
+    int *int_b = (int *)b;
 
     return *int_a - *int_b;
 }
@@ -17,11 +17,41 @@ void sort(int *array, int n)
     qsort(array, n, sizeof(int), compare);
 }
 
+#include <stdbool.h>
+
+bool is_sorted(char *filename)
+{
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL)
+    {
+        return false;
+    }
+
+    int prev_num, curr_num;
+    bool first_num = true;
+
+    while (fscanf(file, "%d", &curr_num) == 1)
+    {
+        if (!first_num && curr_num < prev_num)
+        {
+            fclose(file);
+            return false;
+        }
+
+        prev_num = curr_num;
+        first_num = false;
+    }
+
+    fclose(file);
+    return true;
+}
+
 int main(int argc, char **argv)
 {
-    if (argc < 4)
+    if (argc < 5)
     {
-        printf("Usage: %s <input_filename> <output_filename> <run_count> <run_size>\n", argv[0]);
+        printf("Usage: %s <input_filename> <output_filename> <run_count> <run_size> <buffer_size>\n", argv[0]);
         return 1;
     }
 
@@ -30,8 +60,20 @@ int main(int argc, char **argv)
 
     int run_count = parse_int_or_exit(argv[3]);
     int run_size = parse_int_or_exit(argv[4]);
+    int buffer_size = parse_int_or_exit(argv[5]);
 
-    sort_files(input_filename, output_filename, run_count, run_size, sort);
+    sort_files(input_filename, output_filename, run_count, run_size, sort, buffer_size);
+
+    // debug
+    bool sorted_output = is_sorted(output_filename);
+    if (sorted_output)
+    {
+        printf("true\n");
+    }
+    else
+    {
+        printf("false\n");
+    }
 
     return 0;
 }
