5c6c8c85d3aec6671d04ed4885a182c87111df67
Jaime Daniel <jaimeadanielfilho@hotmail.com>
2024-06-09 20:43:15 -0300

    feat(trabalhos/t1): add more options to sort program

--- trabalhos/t1/include/common/external_sort.h
+++ trabalhos/t1/include/common/external_sort.h
@@ -2,7 +2,20 @@
 #define EXTERNAL_SORT_H
 
 typedef void (*SortFunction)(int *, int);
+typedef struct sort_options SortOptions;
 
-void sort_files(char *input_filename, char *output_filename, int run_count, int run_size, SortFunction sort, int buffer_size);
+struct sort_options
+{
+    SortFunction sort;
+    char *input_filename;
+    char *output_filename;
+    char *tmp_directory;
+    int run_count;
+    int run_size;
+    int buffer_size;
+};
+
+
+void sort_files(SortOptions *options);
 
 #endif
