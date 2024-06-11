e78b6b22f9beef221006584c0c40b0335de5c5ef
Jaime Filho <jaimeadanielfilho@hotmail.com>
2024-06-11 19:44:00 -0300

    feat(trabalhos/t1): add script to plot quicksert samples by threshold

---
+++ trabalhos/t1/scripts/plot_quicksert_threshold.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+input_filename = 'data/quicksert_thresholds.txt'
+
+minimum_threshold = 0
+maximum_threshold = 1000
+
+with open(input_filename, 'r') as f:
+    lines = f.readlines()
+    thresholds = np.arange(minimum_threshold, maximum_threshold + 1, 1)
+    samples = np.array(list(map(lambda line: np.array(list(map(float, line.split(',')))), lines[1:])))
+    averages = np.array(list(map(lambda s : np.average(s, axis=0), samples)))
+
+fig, ax = plt.subplots()
+
+ax.set_title('Vetor com 1e+06 elementos')
+ax.ticklabel_format(style='plain')
+ax.scatter(thresholds, averages,  s=0.1, label='Média dos tempos de execução')
+ax.set_xlabel('Limiar')
+ax.set_ylabel('Tempo de execução (ciclos de clock)')
+ax.xaxis.set_ticks(np.arange(minimum_threshold, maximum_threshold + 1, 100))
+
+print(np.argmin(averages))
+
+plt.show()
